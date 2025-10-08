5dedd3642382e06c34a22521562641efc47e98cf
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2025-10-08 14:09:07 +0200

    ajout des fichiers d'environnement

---
+++ src/utils/env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="astro/client" />
+
+declare namespace App {
+  interface Locals {
+    lang: "en" | "fr";
+    user?: {
+      id: string;
+      email: string;
+    } | null;
+  }
+}
